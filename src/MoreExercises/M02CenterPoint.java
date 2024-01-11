84bce1e452e7b7e3317406d1d78ae44e68b1e158
IvetoIvanova <135642648+IvetoIvanova@users.noreply.github.com>
2024-01-11 23:08:25 +0200

    Added solution of the task "M02CenterPoint".
    Added solution of the task "M02CenterPoint" to the "More exercises" folder of the "SoftUni-Java-Fundamentals-Jan-2023" repository.

---
+++ src/MoreExercises/M02CenterPoint.java
@@ -0,0 +1,26 @@
+package MoreExercises;
+
+import java.util.Scanner;
+
+public class M02CenterPoint {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int x1 = Integer.parseInt(scanner.nextLine());
+        int y1 = Integer.parseInt(scanner.nextLine());
+        int x2 = Integer.parseInt(scanner.nextLine());
+        int y2 = Integer.parseInt(scanner.nextLine());
+        printClosestPoint(x1, y1, x2, y2);
+    }
+
+    public static void printClosestPoint(int x1, int y1, int x2, int y2) {
+        double distance1 = Math.sqrt(x1 * x1 + y1 * y1);
+        double distance2 = Math.sqrt(x2 * x2 + y2 * y2);
+
+        if (distance1 < distance2) {
+            System.out.println("(" + x1 + ", " + y1 + ")");
+        } else {
+            System.out.println("(" + x2 + ", " + y2 + ")");
+        }
+    }
+}
